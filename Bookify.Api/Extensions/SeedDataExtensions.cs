8268e81c48e62fbf8329057a794c7660d8872f3d
milos-code <milos.jovanovic9955@gmail.com>
2023-11-16 15:22:02 +0100

    Implementation of the presentation layer, containing an API and database connection.

---
+++ Bookify.Api/Extensions/SeedDataExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Bogus;
+using Bookify.Application.Abstractions.Data;
+using Bookify.Domain.Apartments;
+
+namespace Bookify.Api.Extensions
+{
+    public static class SeedDataExtensions
+    {
+        public static void SeedData(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+
+            var sqlConnectionFactory = scope.ServiceProvider.GetRequiredService<ISqlConnectionFactory>();
+            using var connection = sqlConnectionFactory.CreateConnection();
+
+            var faker = new Faker();
+
+            List<object> apartments = new();
+            for(var i = 0; i < 100; i++)
+            {
+                apartments.Add(new
+                {
+                    Id = Guid.NewGuid(),
+                    Name = faker.Company.CompanyName(),
+                    Description = "Amazing view",
+                    Country = faker.Address.Country(),
+                    State = faker.Address.State(),
+                    ZipCode = faker.Address.ZipCode(),
+                    City = faker.Address.City(),
+                    Street = faker.Address.StreetAddress(),
+                    PriceAmount = faker.Random.Decimal(50,1000),
+                    PriceCurrnecy = "USD",
+                    CleaningFeeAmount = faker.Random.Decimal(25,200),
+                    CleaningFeeCurrency = "USD",
+                    Amenities = new List<int> { (int)Amenity.Parking, (int)Amenity.MountainView},
+                    LastBookedOn = DateTime.MinValue
+                });
+            }
+        }
+    }
+}
